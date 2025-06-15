de8839448d6eb304e76d8f4d0f9555a2fe4a6fdb
Thai Le <thai.lt194372@sis.hust.edu.vn>
2025-06-16 06:10:41 +0700

    feat: healthcheck

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 
+import { PrismaModule } from '../prisma/prisma.module';
+
 import { HealthController } from './health.controller';
 import { PrismaHealthIndicator } from './prisma.health';
 
 @Module({
-  imports: [TerminusModule],
+  imports: [TerminusModule, PrismaModule],
   controllers: [HealthController],
   providers: [PrismaHealthIndicator],
 })
